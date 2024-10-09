311d6df9f76fe72077a769944ac5cc68ee1b1524
jeferson <jefersoun.silva@gmail.com>
2024-10-09 18:10:51 -0300

    feature: Initial Setup Application, Logs, Postgresql and dependencies.

---
+++ src/main/java/com/compass/challenger/PaymentRoutingApplication/PaymentRoutingApplication.java
@@ -0,0 +1,13 @@
+package com.compass.challenger.PaymentRoutingApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentRoutingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaymentRoutingApplication.class, args);
+	}
+
+}
