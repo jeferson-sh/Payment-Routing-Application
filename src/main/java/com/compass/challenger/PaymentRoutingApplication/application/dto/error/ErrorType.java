7cc45c1eedc97eb6e545360fdc37fc1efb017afb
jeferson <jefersoun.silva@gmail.com>
2024-10-10 13:40:05 -0300

    feature: request and responses finish

---
+++ src/main/java/com/compass/challenger/PaymentRoutingApplication/application/dto/error/ErrorType.java
@@ -0,0 +1,9 @@
+package com.compass.challenger.PaymentRoutingApplication.application.dto.error;
+
+public enum ErrorType {
+    SELLER_NOT_FOUND,
+    PAYMENT_ITEM_NOT_FOUND,
+    SEND_MESSAGE_ERROR,
+    VALIDATION_ERROR,
+    GENERIC_ERROR
+}
